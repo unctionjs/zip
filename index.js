5573f11b06378647d289d74e1bf9c673a1272b65
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:28:13 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-import fresh from "@unction/fresh"
-import keys from "@unction/keys"
-import key from "@unction/key"
-import {uniq} from "ramda"
-import type from "@unction/type"
-import attach from "@unction/attach"
-import reduceValues from "@unction/reducevalues"
-
-export default function zip (left: ArrayType | ObjectType): Function {
-  return function zipLeft (right: ArrayType | ObjectType): ArrayType | ObjectType {
+import fresh from "@unction/fresh";
+import keys from "@unction/keys";
+import key from "@unction/key";
+import { uniq } from "ramda";
+import type from "@unction/type";
+import attach from "@unction/attach";
+import reduceValues from "@unction/reducevalues";
+export default function zip(left) {
+  return function zipLeft(right) {
     if (type(left) !== type(right)) {
-      throw new Error("left and right were not the same functor type")
+      throw new Error("left and right were not the same functor type");
     }
 
-    const uniqueKeys = uniq([...keys(left), ...keys(right)])
-
-    return reduceValues((accumulated: FunctorType): Function => (point: KeyType): FunctorType => {
-      return attach(point)([key(point)(left), key(point)(right)])(accumulated)
-    })(
-      fresh(right)
-    )(
-      uniqueKeys
-    )
-  }
+    const uniqueKeys = uniq([...keys(left), ...keys(right)]);
+    return reduceValues(accumulated => point => {
+      return attach(point)([key(point)(left), key(point)(right)])(accumulated);
+    })(fresh(right))(uniqueKeys);
+  };
 }
