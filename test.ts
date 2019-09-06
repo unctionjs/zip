4eb1919d26f0ae486020ffdfc3aa480b0cd708c2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:56 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,30 @@
+import zip from "./index.ts";
+
+test(() => {
+  expect(zip(["1", "2", "3"])(["4", "5", "6"])).toEqual([["1", "4"], ["2", "5"], ["3", "6"]]);
+});
+
+test(() => {
+  expect(zip({
+    aaa: "1",
+    bbb: "2",
+    ccc: "0",
+  })({
+    aaa: "0",
+    bbb: "0",
+    ccc: "0",
+  })).toEqual({
+    aaa: ["1", "0"],
+    bbb: ["2", "0"],
+    ccc: ["0", "0"],
+  });
+});
+
+test(() => {
+  expect(() =>
+    zip({
+      aaa: "1",
+      bbb: "2",
+      ccc: "0",
+    })(["4", "5", "6"])).toThrow();
+});
